079baaf830b6186d2df26480efbbc09cb2824f28
wwx <84833397+wwxstudy@users.noreply.github.com>
2023-07-01 16:59:34 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o7zrQ6qqRrbf3NfGKnVNbA6cm3CM',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'dzR5hI1y7mncGLe3b5h5dCuC5lJqHN5pyjMZ5lojxRo',
+      useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '08-05',
       festivals: [
@@ -49,7 +49,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'dzR5hI1y7mncGLe3b5h5dCuC5lJqHN5pyjMZ5lojxRo',
+  CALLBACK_TEMPLATE_ID: '0002',
 
   CALLBACK_USERS: [
     {

e5281b86f2e5fb9e20c7f7483c1657ae7e790ad9
wwx <84833397+wwxstudy@users.noreply.github.com>
2023-07-01 17:13:20 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'o7zrQ6qqRrbf3NfGKnVNbA6cm3CM',
+      id: 'o7zrQ6i9okquf6A-GyaaWOT6B7MQ',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'dzR5hI1y7mncGLe3b5h5dCuC5lJqHN5pyjMZ5lojxRo',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
